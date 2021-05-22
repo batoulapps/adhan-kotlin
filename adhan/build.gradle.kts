ae8a3acdec85b1435ce3fe114bcb155b5773d18c
Ahmed El-Helw <ahmedre@gmail.com>
2021-05-22 19:46:12 +0400

    Update dependencies

--- adhan/build.gradle.kts
+++ adhan/build.gradle.kts
@@ -1,10 +1,23 @@
 plugins {
     kotlin("multiplatform")
-    kotlin("plugin.serialization") version "1.4.30"
+    kotlin("plugin.serialization") version "1.5.0"
 }
 
 kotlin {
-    jvm()
+    jvm {
+        val main by compilations.getting {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+
+        val test by compilations.getting {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+
     val onPhone = System.getenv("SDK_NAME")?.startsWith("iphoneos") ?: false
     if (onPhone) {
         iosArm64("ios")
@@ -14,20 +27,20 @@ kotlin {
 
     sourceSets["commonMain"].dependencies {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
-        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.1.1")
+        implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.2.0")
     }
 
     sourceSets["commonTest"].dependencies {
         implementation(kotlin("test-common"))
         implementation(kotlin("test-annotations-common"))
-        api("com.squareup.okio:okio-multiplatform:3.0.0-alpha.1")
-        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.1.0")
+        api("com.squareup.okio:okio-multiplatform:3.0.0-alpha.5")
+        implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
     }
 
     sourceSets["jvmTest"].dependencies {
         dependencies {
             implementation(kotlin("test-junit"))
-            implementation("junit:junit:4.12")
+            implementation("junit:junit:4.13.2")
             implementation(kotlin("stdlib-jdk8"))
         }
     }
