2164e7b78e401de9d11414ac1ba27c2d403cb402
Ahmed El-Helw <ahmedre@gmail.com>
2021-06-15 02:50:43 +0400

    Support publishing to Sonatype

--- adhan/build.gradle.kts
+++ adhan/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform")
     kotlin("plugin.serialization") version "1.5.10"
     id("maven-publish")
+    id("signing")
 }
 
 group = "com.batoulapps.adhan"
@@ -53,10 +54,85 @@ kotlin {
     sourceSets["iosTest"].dependencies {  }
 }
 
+// taken from here with minor modifications:
+// https://dev.to/kotlin/how-to-build-and-publish-a-kotlin-multiplatform-library-going-public-4a8k
+// planning on moving this to a plugin one day.
+
+// Stub secrets to let the project sync and build without the publication values set up
+ext["signing.keyId"] = project.property("signing.keyId")
+ext["signing.password"] = project.property("signing.password")
+ext["signing.secretKeyRingFile"] = project.property("signing.secretKeyRingFile")
+ext["ossrhUsername"] = project.property("mavenCentralRepositoryUsername")
+ext["ossrhPassword"] = project.property("mavenCentralRepositoryPassword")
+
+val javadocJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("javadoc")
+}
+
+fun getExtraString(name: String) = ext[name]?.toString()
+
 publishing {
-    // TODO: see something like this for publishing:
-    //  https://dev.to/kotlin/how-to-build-and-publish-a-kotlin-multiplatform-library-going-public-4a8k
     publications.withType<MavenPublication> {
         artifactId = artifactId.replace("adhan", "adhan2")
     }
+
+    // Configure maven central repository
+    repositories {
+        maven {
+            name = "snapshots"
+            setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+            mavenContent {
+                snapshotsOnly()
+            }
+        }
+
+        maven {
+            name = "sonatype"
+            setUrl("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = getExtraString("ossrhUsername")
+                password = getExtraString("ossrhPassword")
+            }
+            mavenContent {
+                releasesOnly()
+            }
+        }
+    }
+
+    // Configure all publications
+    publications.withType<MavenPublication> {
+        // Stub javadoc.jar artifact
+        artifact(javadocJar.get())
+
+        // Provide artifacts information requited by Maven Central
+        pom {
+            name.set("Adhan Prayertimes Library")
+            description.set("A high precision Islamic prayer times library")
+            url.set("https://github.com/batoulapps/adhan-java")
+
+            licenses {
+                license {
+                    name.set("MIT")
+                    url.set("https://opensource.org/licenses/MIT")
+                }
+            }
+            developers {
+                developer {
+                }
+            }
+            scm {
+                url.set("https://github.com/batoulapps/adhan-java")
+            }
+
+        }
+    }
+}
+
+// Signing artifacts. Signing.* extra properties values will be used
+signing {
+    sign(publishing.publications)
 }
