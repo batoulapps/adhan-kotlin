5bf90d3b1a2fbc97f2c967558f2a774e03da62d2
Ahmed El-Helw <ahmedre@gmail.com>
2022-01-24 00:59:21 +0400

    Update to support Kotlin 1.6.10

--- adhan/src/commonTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
+++ adhan/src/commonTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
@@ -1,9 +1,7 @@
 package com.batoulapps.adhan2
 
-import okio.ExperimentalFileSystem
 import okio.FileSystem
 
-@ExperimentalFileSystem
 expect class TestUtil() {
   fun fileSystem(): FileSystem
 }
--- adhan/src/iosTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
+++ adhan/src/iosTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
@@ -1,9 +1,7 @@
 package com.batoulapps.adhan2
 
-import okio.ExperimentalFileSystem
 import okio.FileSystem
 
-@ExperimentalFileSystem
 actual class TestUtil actual constructor() {
   actual fun fileSystem(): FileSystem = FileSystem.SYSTEM
 }
