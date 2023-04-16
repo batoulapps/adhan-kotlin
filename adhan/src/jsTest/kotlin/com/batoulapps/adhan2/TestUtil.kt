3b1fddc6d1fd84919015c9532b2e76b69e005c50
Ahmed El-Helw <ahmedre@gmail.com>
2023-04-16 18:16:13 +0400

    Update dependency versions

--- adhan/src/jsTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
+++ adhan/src/jsTest/kotlin/com/batoulapps/adhan2/TestUtil.kt
@@ -3,6 +3,12 @@ package com.batoulapps.adhan2
 import okio.FileSystem
 import okio.NodeJsFileSystem
 
+@JsModule("@js-joda/timezone")
+@JsNonModule
+external object JsJodaTimeZoneModule
+
+private val jsJodaTz = JsJodaTimeZoneModule
+
 actual class TestUtil actual constructor() {
   actual fun fileSystem(): FileSystem = NodeJsFileSystem
 }
