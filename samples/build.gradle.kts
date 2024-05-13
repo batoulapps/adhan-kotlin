763b8362fa77c4fb6d16e905809b2765300bff35
Ahmed El-Helw <ahmedre@gmail.com>
2024-05-13 21:47:00 +0400

    Fix build

--- samples/build.gradle.kts
+++ samples/build.gradle.kts
@@ -5,24 +5,14 @@ plugins {
   application
 }
 
-kotlin {
-  jvmToolchain {
-    languageVersion.set(JavaLanguageVersion.of(8))
-  }
-}
-
 repositories {
   mavenCentral() 
 }
 
 dependencies {
-  implementation(kotlin("stdlib-jdk8"))
   implementation(project(":adhan"))
 }
 
 application {
  mainClass.set("com.batoulapps.adhan2.Example")
 }
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions { jvmTarget = "1.8" }
